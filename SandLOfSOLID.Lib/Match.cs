3e36b5b3dc298d3d5a933739a9e586136629ae01
Rob Seaver <rob.seaver@gmail.com>
2017-03-01 11:49:17 -0600

    Fix the match class

--- SandLOfSOLID.Lib/Match.cs
+++ SandLOfSOLID.Lib/Match.cs
@@ -1,9 +1,30 @@
-﻿namespace SandLOfSOLID.Lib
+﻿using System;
+using System.Collections.Generic;
+
+namespace SandLOfSOLID.Lib
 {
-	internal class Match
+	public class Match
 	{
+		private List<Fighter> _fighters;
+        
 		public Match()
 		{
+			_fighters = new List<Fighter>();
+		}
+
+		public List<Fighter> Fighters
+		{
+			get { return _fighters; }
+		}
+
+		public void AddFighter(Fighter fighter)
+		{
+			_fighters.Add(fighter);
+		}
+
+		public void Fight()
+		{
+			throw new NotImplementedException("need fighters to do shtuff");
 		}
 	}
 }
