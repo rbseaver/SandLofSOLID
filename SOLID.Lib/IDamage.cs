7d3dbaf2cbdb757d4cc63f7a8d130a1ed7dad8ba
Rob Seaver <rob.seaver@gmail.com>
2017-04-16 18:33:00 -0500

    Start using IDamage. Fuck shit up, bruh!

--- SOLID.Lib/IDamage.cs
+++ SOLID.Lib/IDamage.cs
@@ -2,6 +2,6 @@
 {
     public interface IDamage
     {
-        
+        void Damage();
     }
 }
--- SOLID.Lib/Weapon.cs
+++ SOLID.Lib/Weapon.cs
@@ -1,9 +1,15 @@
-﻿namespace SOLID.Lib
+﻿using System;
+
+namespace SOLID.Lib
 {
-	public class Weapon
-	{
+	public class Weapon : IDamage {
 		public int Speed { get; set; }
 		public int HandsToHold { get; set; }
 		public int HitPower { get; set; }
-	}
+
+        public void Damage()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
